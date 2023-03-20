f1538e2c0383aff5835bed9c10e82e8d5db372e9
miaoji.lym <miaoji.lym@alibaba-inc.com>
2023-03-20 21:58:32 +0800

    [add] projectile auto blueprint

--- Source/ActionRPG/Private/RCharacter.cpp
+++ Source/ActionRPG/Private/RCharacter.cpp
@@ -124,6 +124,7 @@ void ARCharacter::PrimaryAttack_TimeElapsed()
 
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnParams.Instigator = this;
 
 	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
 }
